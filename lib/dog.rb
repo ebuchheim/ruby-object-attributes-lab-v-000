0b3a25ab70f100e26779ce796cb1cc8636b67242
ebuchheim <emma.buchheim@gmail.com>
2019-01-08 10:53:25 -0800

    add attributes

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+
+    def initialize
+        @name = name
+        @breed = breed
+    end
+
+    def name
+        @name
+    end
+
+    def name=(set_name)
+        @name = set_name
+    end
+
+    def breed
+        @breed
+    end
+
+    def breed=(set_breed)
+        @breed = set_breed
+    end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,24 @@
+class Person
+
+    def initialize
+        @name = name
+        @job = job
+    end
+
+    def name
+        @name
+    end
+
+    def name=(set_name)
+        @name = set_name
+    end
+
+    def job
+        @job
+    end
+
+    def job=(set_job)
+        @job = set_job
+    end
+
+end
